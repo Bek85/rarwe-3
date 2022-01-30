cf0ef73de0827f77098891a57ee90890d30198af
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-01-30 17:36:54 +0500

    create isAddButtonDisabled property to prevent creating bands without a name

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -1,10 +1,12 @@
 import Controller from "@ember/controller";
 import { action } from "@ember/object";
+import { empty } from "@ember/object/computed";
 import Band from "rarwe/models/band";
 
 export default Controller.extend({
   isAddingBand: false,
   newBandName: "",
+  isAddButtonDisabled: empty("newBandName"),
 
   addBand: action(function () {
     this.set("isAddingBand", true);
