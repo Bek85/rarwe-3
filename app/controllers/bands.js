c8eea1cb13657c19419a02fe06493b1a1f59dafa
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-01-30 16:51:53 +0500

    modify bands template to accomodate 2 more action handlers and create corresponding ones in the controller

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -3,8 +3,17 @@ import { action } from "@ember/object";
 
 export default Controller.extend({
   isAddingBand: false,
+  newBandName: "",
 
   addBand: action(function () {
     this.set("isAddingBand", true);
+  }),
+
+  cancelAddBand: action(function () {
+    this.set("isAddingBand", false);
+  }),
+
+  saveBand: action(function () {
+    // Create a new band
   }),
 });
