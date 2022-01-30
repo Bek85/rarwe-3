8806d7c0d1de459b53f8e414720f02aee74c390b
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-01-30 17:29:19 +0500

    import Band model into Band controller and use it to create a new band

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -1,5 +1,6 @@
 import Controller from "@ember/controller";
 import { action } from "@ember/object";
+import Band from "rarwe/models/band";
 
 export default Controller.extend({
   isAddingBand: false,
@@ -15,5 +16,7 @@ export default Controller.extend({
 
   saveBand: action(function () {
     // Create a new band
+    let newBand = Band.create({ name: this.newBandName });
+    this.model.pushObject(newBand);
   }),
 });
