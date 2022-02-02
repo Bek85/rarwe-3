9b114a00abc0332033f74032ca103bfa93755087
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-02-03 01:25:34 +0500

    transition to bands.band.songs route after adding a new band and set bands slug property as well

--- app/controllers/bands.js
+++ app/controllers/bands.js
@@ -1,9 +1,12 @@
 import Controller from "@ember/controller";
 import { action } from "@ember/object";
 import { empty } from "@ember/object/computed";
+import { inject as service } from "@ember/service";
+import { dasherize } from "@ember/string";
 import Band from "rarwe/models/band";
 
 export default Controller.extend({
+  router: service(),
   isAddingBand: false,
   newBandName: "",
   isAddButtonDisabled: empty("newBandName"),
@@ -21,6 +24,11 @@ export default Controller.extend({
     // Create a new band
     let newBand = Band.create({ name: this.newBandName });
     this.model.pushObject(newBand);
-    this.set("newBandName", "");
+    this.setProperties({
+      newBandName: "",
+      isAddingBand: false,
+    });
+    newBand.set("slug", dasherize(newBand.name));
+    this.router.transitionTo("bands.band.songs", newBand.slug);
   }),
 });
