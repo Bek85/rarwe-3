3e5983b35c194092745bcef13216280ce8c9c2f0
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-02-08 15:15:03 +0500

    set the rating of a song to zero if the current rating is clikced

--- app/controllers/bands/band/songs.js
+++ app/controllers/bands/band/songs.js
@@ -25,6 +25,6 @@ export default Controller.extend({
   }),
 
   updateRating: action(function (song, rating) {
-    song.set("rating", rating);
+    song.set("rating", song.rating === rating ? 0 : rating);
   }),
 });
