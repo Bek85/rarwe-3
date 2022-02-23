a86a24dad942ad88ebd9518a06a7c146e3f2c74d
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-02-23 21:34:18 +0500

    warn about losing data when trying to navigate to another route

--- app/controllers/bands/band/details.js
+++ app/controllers/bands/band/details.js
@@ -1,10 +1,21 @@
 import Controller from "@ember/controller";
 import { action } from "@ember/object";
+import Band from "rarwe/models/band";
 
 export default Controller.extend({
   isEditing: false,
+  description: "",
 
-  toggleIsEditing: action(function () {
-    this.toggleProperty("isEditing");
-  }),
+  // saveDescription: action(function () {
+
+  //   this.set("isEditing", true);
+  //   let newDescription = Band.create({ description: this.description });
+  //   console.log(newDescription);
+
+  //   this.model.pushObject(newDescription);
+  //   this.setProperties({
+  //     isEditing: false,
+  //     // description: "",
+  //   });
+  // }),
 });
