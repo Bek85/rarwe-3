eb041a541b3e1b97e20bc0e677717a9febd22b4d
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-03-06 20:30:51 +0500

    create a unit test for band model

--- app/models/band.js
+++ app/models/band.js
@@ -1,7 +1,13 @@
 import Model, { attr, hasMany } from "@ember-data/model";
+import { computed } from "@ember/object";
 
 export default Model.extend({
   name: attr(),
   description: attr(),
   songs: hasMany(),
+
+  isGreatBand: computed("songs.@each.rating", function () {
+    let goodSongs = this.get("songs").filter((song) => song.rating >= 4);
+    return goodSongs.length >= 2;
+  }),
 });
