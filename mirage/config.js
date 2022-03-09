df0aa0b0ee9b35305b7da930407d3d726640997a
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-03-09 12:58:10 +0500

    - add new styles - write a test to verify that the songs are sorted in the order selected by the user - create Mirage serializer to turn Mirage models into a JSON representation to send as response - in mirage/serializers/band.js, customize "links" method so that (test) back-end does get hit by the /bands/:id/songs request - add the new handler to Mirage's config as "this.get('bands/:id/songs')" - implement route handler to return all songs related to a particular band in mirage/config.js

--- mirage/config.js
+++ mirage/config.js
@@ -25,7 +25,10 @@ export default function () {
 
   this.get("/bands");
   this.get("/bands/:id");
-  // this.get("bands/:id/songs");
-  this.post("/bands");
+  this.get("/bands/:id/songs", function (schema, request) {
+    let id = request.params.id;
+    return schema.songs.where({ bandId: id });
+  }),
+    this.post("/bands");
   // this.post("/bands/:id");
 }
