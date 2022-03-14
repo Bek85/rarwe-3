7b124781f7fc28d6d23c13da5cc5bb635d33f238
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-03-14 14:49:29 +0500

    restore the original model hooks

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,13 +1,7 @@
 import Route from "@ember/routing/route";
-import wait from "rarwe/utils/wait";
 
 export default Route.extend({
-  // model() {
-  //   return this.store.findAll("band");
-  // },
-
-  async model() {
-    await wait(3000);
+  model() {
     return this.store.findAll("band");
   },
 });
--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,9 +1,7 @@
 import Route from "@ember/routing/route";
-import wait from "rarwe/utils/wait";
 
 export default Route.extend({
-  async model() {
-    await wait(3000);
+  model() {
     return this.modelFor("bands.band");
   },
 
