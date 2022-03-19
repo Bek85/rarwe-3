745767779302dcdaafa017a83315e0797ecd43fc
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-03-19 22:10:50 +0500

    enable validation checks  for email and password fields

--- app/controllers/sign-up.js
+++ app/controllers/sign-up.js
@@ -5,6 +5,17 @@ import { action } from "@ember/object";
 export default Controller.extend({
   router: service(),
 
+  init() {
+    this._super(...arguments);
+    this.set("showErrors", { email: false, password: false });
+  },
+
+  setShowErrors: action(function (property) {
+    let showErrors = { ...this.showErrors };
+    showErrors[property] = true;
+    this.set("showErrors", showErrors);
+  }),
+
   signUp: action(async function (evt) {
     evt.preventDefault();
     let { email, password } = this;
