3b4253b987132b4af47daafdcd545c27cdde387c
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-03-19 21:53:20 +0500

    create validations for user model's email and password attributes

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,33 @@
 import Model, { attr } from "@ember-data/model";
+import { buildValidations, validator } from "ember-cp-validations";
 
-export default Model.extend({
+const Validations = buildValidations({
+  email: [
+    validator("presence", {
+      presence: true,
+      ignoreBlank: true,
+      message: "Email can't be empty",
+    }),
+    validator("format", {
+      type: "email",
+      message: "Email should be a valid email",
+    }),
+  ],
+
+  password: [
+    validator("presence", {
+      presence: true,
+      ignoreBlank: true,
+      message: "Password can't be empty",
+    }),
+    validator("length", {
+      min: 8,
+      message: "Password should be at least 8 characters",
+    }),
+  ],
+});
+
+export default Model.extend(Validations, {
   email: attr(),
   password: attr(),
 });
