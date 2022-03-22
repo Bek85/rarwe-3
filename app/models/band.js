64047ab494a5909fed9359337a1da05645474afe
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-03-22 17:43:33 +0500

    update details page and use custom validator in band model to validate text area field

--- app/models/band.js
+++ app/models/band.js
@@ -1,7 +1,25 @@
 import Model, { attr, hasMany } from "@ember-data/model";
 import { computed } from "@ember/object";
+import { buildValidations, validator } from "ember-cp-validations";
 
-export default Model.extend({
+const Validations = buildValidations({
+  name: [
+    validator("presence", {
+      presence: true,
+      ignoreBlank: true,
+      message: "Name can't be empty",
+    }),
+  ],
+  description: [
+    validator("length", {
+      min: 12,
+      message: "The description needs to be at least 12 characters",
+    }),
+    validator("year-of-formation"),
+  ],
+});
+
+export default Model.extend(Validations, {
   name: attr(),
   description: attr(),
   songs: hasMany(),
