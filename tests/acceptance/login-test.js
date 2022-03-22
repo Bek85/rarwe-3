fb8d190a63de496858374d00c1ecc0996680f6de
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-03-22 15:21:20 +0500

    add a test to check the input field validation errors flow in login page

--- tests/acceptance/login-test.js
+++ tests/acceptance/login-test.js
@@ -4,7 +4,7 @@ import {
   fillIn,
   click,
   currentURL,
-  pauseTest,
+  triggerEvent,
 } from "@ember/test-helpers";
 import { setupApplicationTest } from "ember-qunit";
 import { setupMirage } from "ember-cli-mirage/test-support";
@@ -38,5 +38,34 @@ module("Acceptance | login", function (hooks) {
 
     // assert.dom("[data-test-rr=form-header]").hasText("Log in to R&R");
     assert.dom("[data-test-rr=user-email]").doesNotExist();
+  });
+
+  test("Login client-side errors", async function (assert) {
+    await visit("/login");
+
+    await fillIn("#email", "dave#tcv.com");
+    await triggerEvent("#email", "blur");
+    assert
+      .dom("[data-test-rr=email-error]")
+      .hasText("Email should be a valid email", "Email error is displayed");
+
+    await fillIn("#password", "crooked");
+    await triggerEvent("#password", "blur");
+    assert
+      .dom("[data-test-rr=password-error]")
+      .hasText(
+        "Password should be at least 8 characters",
+        "Password error is displayed"
+      );
+
+    await fillIn("#email", "dave@tcv.com");
+    assert
+      .dom("[data-test-rr=email-error]")
+      .hasText("", "Email error is no longer displayed");
+
+    await fillIn("#password", "ThemCr00ked!");
+    assert
+      .dom("[data-test-rr=password-error]")
+      .hasText("", "Password error is no longer displayed");
   });
 });
