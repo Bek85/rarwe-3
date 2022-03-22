e623901daa58980cb8c56396f41722e44e6d84ae
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-03-22 15:38:06 +0500

    implement email and password field validations in login page

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,10 +1,29 @@
 import Controller from "@ember/controller";
+import { buildValidations } from "ember-cp-validations";
 import { inject as service } from "@ember/service";
 import { action } from "@ember/object";
+import emailField from "rarwe/validations/email-field";
+import passwordField from "rarwe/validations/password-field";
 
-export default Controller.extend({
+const Validations = buildValidations({
+  email: emailField,
+  password: passwordField,
+});
+
+export default Controller.extend(Validations, {
   router: service(),
   session: service(),
+
+  init() {
+    this._super(...arguments);
+    this.set("showErrors", { email: false, password: false });
+  },
+
+  setShowErrors: action(function (property) {
+    let showErrors = { ...this.showErrors };
+    showErrors[property] = true;
+    this.set("showErrors", showErrors);
+  }),
 
   signIn: action(async function (evt) {
     evt.preventDefault();
