9ba26aeb71995ad48ab106128ca0ca5a56e54d0a
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-05-09 14:34:35 +0500

    create "initials" computed property in Musicians class

--- app/models/musician.js
+++ app/models/musician.js
@@ -1,5 +1,6 @@
 import Model, { attr } from "@ember-data/model";
 import { buildValidations, validator } from "ember-cp-validations";
+import { computed } from "@ember/object";
 
 const Validations = buildValidations({
   name: validator("presence", {
@@ -10,4 +11,11 @@ const Validations = buildValidations({
 
 export default Model.extend(Validations, {
   name: attr(),
+
+  initials: computed("name", function () {
+    return this.name
+      .split(/\s+/)
+      .map((namePart) => namePart.charAt(0))
+      .join("");
+  }),
 });
