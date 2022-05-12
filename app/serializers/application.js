d1faf60865fd70cef2b5b92f49a4fbb54d4ec98a
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-05-13 01:31:37 +0500

    transform "yearOfBirth" attribute to "birth-date" sent to backend in serialize hook

--- app/serializers/application.js
+++ app/serializers/application.js
@@ -13,4 +13,16 @@ export default JSONAPISerializer.extend({
     }
     return extracted;
   },
+
+  serialize(snapshot) {
+    let json = this._super(...arguments);
+    if (snapshot.modelName !== "musician") {
+      return json;
+    }
+
+    let yob = snapshot.attributes().yearOfBirth;
+    json.data.attributes["birth-date"] = `${yob}-01-01`;
+    delete json.data.attributes["year-of-birth"];
+    return json;
+  },
 });
