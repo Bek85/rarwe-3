958730677881617b1ad9df2ae36d60498f859d2b
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-06-08 17:54:35 +0500

    add functinality to edit band name

--- app/components/band-form.js
+++ app/components/band-form.js
@@ -1,17 +1,14 @@
 import Component from "@ember/component";
 import { or } from "@ember/object/computed";
 import { action } from "@ember/object";
+import { inject as service } from "@ember/service";
+import { task } from "ember-concurrency";
 
 export default Component.extend({
+  router: service(),
   tagName: "",
   band: null,
   isButtonDisabled: or("band.validations.isInvalid", "saveBand.isRunning"),
-
-  setShowErrors: action(function (property) {
-    let showErrors = { ...this.showErrors };
-    showErrors[property] = true;
-    this.set("showErrors", showErrors);
-  }),
 
   init() {
     this._super(...arguments);
@@ -20,4 +17,16 @@ export default Component.extend({
       description: false,
     });
   },
+
+  setShowErrors: action(function (property) {
+    let showErrors = { ...this.showErrors };
+    showErrors[property] = true;
+    this.set("showErrors", showErrors);
+  }),
+
+  saveBand: task(function* (evt) {
+    evt.preventDefault();
+    yield this.band.save();
+    yield this.router.transitionTo("bands.band.details", this.band.id);
+  }),
 });
